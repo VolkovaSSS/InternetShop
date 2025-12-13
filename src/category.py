b37b00e1a1106cffe12b59d8d3c64b8f086f391d
Sv <VolkovaSVG@mail.ru>
2025-12-14 01:52:20 +0400

    Тесты

--- src/category.py
+++ src/category.py
@@ -1,5 +1,6 @@
 from src.product import Product
 
+
 class Category:
     """Класс для описания категорий продуктов"""
 
@@ -16,21 +17,19 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
-
     def add_product(self, product: Product):
         self.__products.append(product)
         Category.product_count += 1
-
 
     @property
     def products(self):
         products_str = ""
         for product in self.__products:
-            products_str += f'{product.name}, {product.price} руб. {product.quantity} шт\n'
+            products_str += (
+                f"{product.name}, {product.price} руб. {product.quantity} шт\n"
+            )
         return products_str
 
-
-
-
-
-
+    @property
+    def products_in_list(self):
+        return self.__products
--- src/product.py
+++ src/product.py
@@ -12,22 +12,17 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
-
     @classmethod
     def new_product(cls, product_params: dict):
         return cls(**product_params)
-
 
     @property
     def price(self):
         return self.__price
 
-
     @price.setter
     def price(self, price):
         if price <= 0:
-            print('Цена не должна быть нулевая или отрицательная')
-            return 
+            print("Цена не должна быть нулевая или отрицательная")
+            return
         self.__price = price
-
-
